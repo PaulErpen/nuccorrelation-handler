310d12d3e2990f1ea6395333c3dfb935e372dc01
Paul Erpenstein <paul.erpenstein@gmail.com>
2023-01-15 17:02:56 +0100

    change setup.py versions

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     packages=find_packages(include=["nuccorrelation"]),
     test_suite='tests_package',
     install_requires=[
-        "matplotlib>=3.6.2",
+        "matplotlib>=3.6.3",
         "matplotlib-inline>=0.1.6",
-        "numpy>=1.23.0",
+        "numpy>=1.22.4",
         "openpyxl>=3.0.10",
         "pandas>=1.5.2",
         "seaborn>=0.12.1"
